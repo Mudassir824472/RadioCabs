fc046e302540d60d07e62c18658e50ffceb2ac99
Mudassir824472 <mudassir12ali@gmail.com>
2026-02-06 01:08:37 +0500

    Driver Registered

---
+++ RadioCabs/Migrations/20260205101730_AddDriverTable.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RadioCabs.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDriverTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telephone",
+                table: "Drivers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Drivers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CompanyId",
+                table: "Drivers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drivers_CompanyId",
+                table: "Drivers",
+                column: "CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Drivers_Companies_CompanyId",
+                table: "Drivers",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "CompanyId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Drivers_Companies_CompanyId",
+                table: "Drivers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Drivers_CompanyId",
+                table: "Drivers");
+
+            migrationBuilder.DropColumn(
+                name: "CompanyId",
+                table: "Drivers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telephone",
+                table: "Drivers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Drivers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
